5f791c6311f5fd1986f11b87867658782f905a1d
kowada <t.kowada.sys24@morijyobi.ac.jp>
2025-06-06 14:45:12 +0900

    first commit

---
+++ bbs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import  reverse
+# Create your models here.
+class Article(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    uppdated_at = models.DateTimeField(auto_now=True)
+    
+    def get_absolute_url(self):
+        return reverse("bbs:detail", kwargs={"pk": self.pk})
+    
+    def __str__(self):
+        return self.content
